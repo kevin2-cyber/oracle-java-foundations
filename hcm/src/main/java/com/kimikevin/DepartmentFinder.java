46ba772320f1bb48b13227464f68b65cd2d8b8c4
Kevin2-cyber <kieduful47@gmail.com>
2025-05-26 10:52:04 +0000

    Human Resource Use Case
    although everything prints to a null object will fix it in due time

---
+++ hcm/src/main/java/com/kimikevin/DepartmentFinder.java
@@ -0,0 +1,11 @@
+package com.kimikevin;
+
+public class DepartmentFinder {
+    private int number;
+
+    public static Department find(int number) {
+        Department department = new Department();
+        return department;
+    }
+}
+
---
+++ hcm/src/main/java/com/kimikevin/Employee.java
@@ -0,0 +1,14 @@
+package com.kimikevin;
+
+public class Employee {
+    private String name;
+    private Department departmentAssignedTo;
+
+    public String getName() {
+        return name;
+    }
+
+    public Department getDepartment() {
+        return departmentAssignedTo;
+    }
+}
