9a96620d6bc196aa62ee8f3c60bf04e16420159c
Kevin2-cyber <kieduful47@gmail.com>
2025-05-27 13:16:27 +0000

    Pinky

--- ShopApp/src/main/java/duke/choice/Main.java
+++ ShopApp/src/main/java/duke/choice/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Duke Choice Shop");
         Customer c1 = new Customer();
-        c1.name = "Duke";
+        c1.name = "Pinky";
+        System.out.println("Customer is " + c1.name);
     }
 }
