79f98beec67573f5e7c82a88da83a0aced99562b
Kevin2-cyber <kieduful47@gmail.com>
2025-05-27 13:13:35 +0000

    printed using Duke

--- ShopApp/src/main/java/duke/choice/Main.java
+++ ShopApp/src/main/java/duke/choice/Main.java
@@ -3,5 +3,7 @@ package duke.choice;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Duke Choice Shop");
+        Customer c1 = new Customer();
+        c1.name = "Duke";
     }
 }
