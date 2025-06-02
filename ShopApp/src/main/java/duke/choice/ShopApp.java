d28e32e07d16dd137839a2b32fa9ccbb3a628def
Kevin2-cyber <kieduful47@gmail.com>
2025-06-02 00:35:36 +0000

    Cloud Development demo

--- ShopApp/src/main/java/duke/choice/ShopApp.java
+++ ShopApp/src/main/java/duke/choice/ShopApp.java
@@ -7,16 +7,18 @@ import io.helidon.webserver.WebServer;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
-
-import static duke.choice.Clothing.TAX_RATE;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ShopApp {
+    public static Logger logger = Logger.getLogger(ShopApp.class.getName());
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Duke Choice Shop");
         Customer c1 = new Customer("Pinky",3);
 
-        System.out.println("Minimum price: " + TAX_RATE);
+//        System.out.println("Minimum price: " + TAX_RATE);
 
         Clothing item1 = new Clothing("Blue Jacket",20.9,"M");
         Clothing item2 = new Clothing("Orange T-Shirt",10.5,"S");
@@ -41,13 +43,13 @@ public class ShopApp {
             WebServer ws = WebServer.create(config, routing);
             ws.start();
         } catch (UnknownHostException ex) {
-            ex.printStackTrace();
+            logger.log(Level.INFO, ex.getMessage(), ex);
         }
 
-        System.out.println("Customer is " + c1.getName() + "," + c1.getSize() + "," + c1.getTotalClothingCost());
-        for(Clothing item : c1.getItems()) {
-            System.out.println("Item output " + item);
-        }
+//        System.out.println("Customer is " + c1.getName() + "," + c1.getSize() + "," + c1.getTotalClothingCost());
+//        for(Clothing item : c1.getItems()) {
+//            System.out.println("Item output " + item);
+//        }
 
         int average = 0;
         int count = 0;
@@ -62,14 +64,14 @@ public class ShopApp {
         try {
             average = (count == 0) ? 0 : (average / count);
             average = average / count;
-            System.out.println("Average price: " + average + ", Count: " + count);
+//            System.out.println("Average price: " + average + ", Count: " + count);
         } catch (ArithmeticException e) {
-            System.out.println("Don't divide by zero");
+//            System.out.println("Don't divide by zero");
         }
 
         Arrays.sort(c1.getItems());
-        for(Clothing item : c1.getItems()) {
-            System.out.println("Item output " + item);
-        }
+//        for(Clothing item : c1.getItems()) {
+//            System.out.println("Item output " + item);
+//        }
     }
 }
