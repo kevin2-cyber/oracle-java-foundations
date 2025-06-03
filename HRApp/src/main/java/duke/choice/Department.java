bcf57865459535bc281e5f65ab6dd644b69c0cd9
Kevin2-cyber <kieduful47@gmail.com>
2025-06-03 10:10:50 +0000

    fixing bugs

--- HRApp/src/main/java/duke/choice/Department.java
+++ HRApp/src/main/java/duke/choice/Department.java
@@ -3,21 +3,10 @@ package duke.choice;
 public class Department {
     private String name;
     private Employee[] employees = new Employee[10];
-    private int lastAddedEmployeeId = -1;
+    private int lastAddedEmployeeIndex = -1;
 
     public Department(String name) {
         this.name = name;
-    }
-
-    public void addEmployee(Employee employee) {
-        if (lastAddedEmployeeId < employees.length) {
-            lastAddedEmployeeId++;
-            employees[lastAddedEmployeeId] = employee;
-        }
-    }
-
-    public Employee[] getAllEmployees() {
-        return employees;
     }
 
     public String getName() {
@@ -27,6 +16,95 @@ public class Department {
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     Increment lastAddedEmployeeIndex and add employee to this position in the employees array.
+     When adding new employee to the employees array, consider the length of the array.
+     You can simply stop adding extra employees once you have reached your employee array capacity
+     */
+    public void addEmployee(Employee employee) {
+        if (lastAddedEmployeeIndex < employees.length) {
+            lastAddedEmployeeIndex++;
+            employees[lastAddedEmployeeIndex] = employee;
+        }
+    }
+
+    /**
+     The idea of this algorithm is to return an array of employees that contains the exact number
+     of elements that matches the actual number of employees in this department.
+     This can be calculated by adding one to the lastAddedEmployeeIndex variable.
+     You then need to copy references for relevant employee object from the employees array to this new
+     array and return it to the invoker.
+     This way invoker always gets an array that is already trimmed to exact required size.
+     */
+    public Employee[] getEmployees() {
+        Employee[] actualEmployees = new Employee[lastAddedEmployeeIndex + 1];
+
+        for (int i = 0; i < actualEmployees.length; i++) {
+            actualEmployees[i] = employees[i];
+        }
+        return actualEmployees;
+    }
+
+    /**
+     Variable lastAddedEmployeeIndex indicates last valid position in the employees array.
+     Because array indexing starts at zero, you need to add one to get the number of elements.
+     */
+    public int getEmployeeCount() {
+        return lastAddedEmployeeIndex + 1;
+    }
+
+    // query method that attempts to locate an employee with specific id in this department.
+    public Employee getEmployeeById(int empId) {
+        for (Employee emp: employees) {
+            if (emp.getId() == empId) {
+                return emp;
+            }
+        }
+        return null;
+    }
+
+    /**
+     You may reserve a local variable, assign employee object to this variable
+     and break out of the loop, when required employee has been located.
+     */
+    public Employee getEmpById(int empId) {
+        Employee result = null;
+        for (Employee emp: employees) {
+            if (emp.getId() == empId) {
+                result = emp;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     Because employees array may be partially filled values, you need to start at position zero
+     and stop at the element indicated by the lastAddedEmployeeIndex variable.
+     Calculate running total value and return the result.
+     */
+    public double getTotalSalary() {
+        double totalSalary = 0.0;
+        for (int i = 0; i <= lastAddedEmployeeIndex; i++) {
+            totalSalary += employees[i].getSalary();
+        }
+        return totalSalary;
+    }
+
+    /**
+     To calculate average salary you need to calculate total and divide it by the number of elements.
+     You already have a method that does total calculation.
+     You can find out the number of employees in the array by adding one to the lastAddedEmployeeIndex variable.
+     If there are no employees in the array then you can return zero as the average does not make much sense in this case;
+     */
+    public double getAverageSalary() {
+        if (lastAddedEmployeeIndex > -1) {
+            return getTotalSalary() / (lastAddedEmployeeIndex + 1);
+        }
+        return 0;
+    }
+
 
     @Override
     public String toString() {
--- HRApp/src/main/java/duke/choice/HRApp.java
+++ HRApp/src/main/java/duke/choice/HRApp.java
@@ -4,9 +4,34 @@ public class HRApp {
     public static void main(String[] args) {
         System.out.println("HRApp starts");
 
-        Employee employee1 = new Employee(120, "John Doe", 1000.00);
-        Employee employee2 = new Employee(125, "Grace Doe", 2000.00);
-        System.out.println(employee1);
-        System.out.println(employee2);
+        Employee education1 = new Employee(120, "John Doe", 1000.00);
+        Employee education2 = new Employee(125, "Grace Doe", 2000.00);
+
+        Department education = new Department("Education");
+        System.out.println(education);
+        education.addEmployee(education1);
+        education.addEmployee(education2);
+
+        Employee[] educationEmployees = education.getEmployees();
+        for (Employee emp : educationEmployees) {
+            System.out.println(emp);
+        }
+        System.out.println("total salary: " + education.getTotalSalary());
+        System.out.println("average salary: " + education.getAverageSalary());
+
+        Employee computer1 = new Employee(130, "Cole", 3000.00);
+        Employee computer2 = new Employee(135, "Jack", 4000.00);
+
+        Department computer = new Department("Computer");
+        System.out.println(computer);
+        computer.addEmployee(computer1);
+        computer.addEmployee(computer2);
+
+        Employee[] computerEmployees = computer.getEmployees();
+        for (Employee emp : computerEmployees) {
+            System.out.println(emp);
+        }
+        System.out.println("total salary: " + computer.getTotalSalary());
+        System.out.println("average salary: " + computer.getAverageSalary());
     }
 }
