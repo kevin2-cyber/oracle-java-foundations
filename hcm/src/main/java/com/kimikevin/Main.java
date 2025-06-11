100ea14776185bf66d9739a915376a38e1e48ba5
Kevin2-cyber <kieduful47@gmail.com>
2025-06-11 17:20:39 +0000

    tried something

--- hcm/src/main/java/com/kimikevin/Main.java
+++ hcm/src/main/java/com/kimikevin/Main.java
@@ -1,5 +1,7 @@
 package com.kimikevin;
 
+import java.util.Scanner;
+
 ////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 //// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 //public class Main {
@@ -18,10 +20,30 @@ package com.kimikevin;
 
 public class Main {
     public static void main(String[] args) {
-        int departNumber = 1;
-        Employee newEmp = new Employee();
-        Department aDept = DepartmentFinder.find(departNumber);
-        aDept.assignEmployee(newEmp);
-        System.out.println(aDept.getEmployees());
+//        int departNumber = 1;
+//        Employee newEmp = new Employee();
+//        Department aDept = DepartmentFinder.find(departNumber);
+//        aDept.assignEmployee(newEmp);
+//        System.out.println(aDept.getEmployees());
+        // Create a Scanner object to read input
+        Scanner sc = new Scanner(System.in);
+
+        // Read the integer
+        int age = sc.nextInt();
+
+        // Write your code here to determine the life stage based on the input age
+        // You should use if...else if...else statement
+        if(age <= 12) {
+            System.out.println("Child");
+        } else if(age >= 13 && age <= 17) {
+            System.out.println("Teenager");
+        }  else if(age >= 18 && age <= 56) {
+            System.out.println("Adult");
+        } else if(age >= 60){
+            System.out.println("Senior Citizen");
+        }
+        // Then, print out the appropriate message
+
+        sc.close();
     }
 }
