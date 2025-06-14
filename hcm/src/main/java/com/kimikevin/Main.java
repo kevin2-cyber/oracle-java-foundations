2b00e53ce608999f008ac31eeb07296113a0ce7c
Kevin2-cyber <kieduful47@gmail.com>
2025-06-14 11:18:33 +0000

    removing

--- hcm/src/main/java/com/kimikevin/Main.java
+++ hcm/src/main/java/com/kimikevin/Main.java
@@ -2,9 +2,6 @@ package com.kimikevin;
 
 import java.util.Scanner;
 
-////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 
 public class Main {
     public static void main(String[] args) {
