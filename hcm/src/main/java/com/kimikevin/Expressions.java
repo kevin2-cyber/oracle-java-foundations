95a3c4647432021b01fd1ef2bf659a1b904c0cbb
Kevin2-cyber <kieduful47@gmail.com>
2025-06-15 21:58:23 +0000

    expressions

---
+++ hcm/src/main/java/com/kimikevin/Expressions.java
@@ -0,0 +1,14 @@
+package com.kimikevin;
+
+public class Expressions {
+    public static void main(String[] args) {
+        int a = 5, b = 11;
+        System.out.println(b + a);
+        System.out.println(b - a);
+        System.out.println(b * a);
+        System.out.println(b / a);
+        System.out.println(b % a);
+        System.out.println((a - b) + a * (b - a) - a % b);
+        System.out.println((-5) * (-10) * (-20));
+    }
+}
