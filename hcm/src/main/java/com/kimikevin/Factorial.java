37f3c8ae28ba31d93a4f2b86baf4482d3fccc087
Kevin2-cyber <kieduful47@gmail.com>
2025-06-18 16:06:36 +0000

    factorial  using while loop

--- hcm/src/main/java/com/kimikevin/Factorial.java
+++ hcm/src/main/java/com/kimikevin/Factorial.java
@@ -6,15 +6,24 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long n = Integer.parseInt(scanner.nextLine().trim());
-        System.out.println(factorial(n));
-    }
-    public static long factorial(long n) {
-        // write your code here
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
+//        long n = Integer.parseInt(scanner.nextLine().trim());
+//        System.out.println(factorial(n));
+        int input = scanner.nextInt();
+        int i = 1;
+        long factorial = 1;
+
+        while (i <= input) {
+            factorial *= i;
+            i++;
         }
+        System.out.println(factorial);
     }
+//    public static long factorial(long n) {
+//        // write your code here
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        } else {
+//            return n * factorial(n - 1);
+//        }
+//    }
 }
