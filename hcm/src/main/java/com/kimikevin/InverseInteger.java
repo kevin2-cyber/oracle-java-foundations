79dbfa00c86460b30abec96fbfbd68c470c2ea4a
Kevin2-cyber <kieduful47@gmail.com>
2025-06-24 15:12:29 +0000

    reverse integer array

---
+++ hcm/src/main/java/com/kimikevin/InverseInteger.java
@@ -0,0 +1,29 @@
+package com.kimikevin;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InverseInteger {
+    public static void sort(int[] numbers) {
+        // write your code here
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        String[] values = scanner.nextLine().split("\\s+");
+        int[] numbers = Arrays.stream(values)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        sort(numbers);
+        Arrays.stream(numbers).forEach(e -> System.out.print(e + " "));
+    }
+}
