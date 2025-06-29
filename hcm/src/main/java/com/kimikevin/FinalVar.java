cb7532265c7ac860c7fa16c2dc9f7d2b0c678626
Kevin2-cyber <kieduful47@gmail.com>
2025-06-29 12:10:11 +0000

    constants

---
+++ hcm/src/main/java/com/kimikevin/FinalVar.java
@@ -0,0 +1,11 @@
+package com.kimikevin;
+
+public class FinalVar {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        final String HELLO_MSG = "Hello";
+
+        System.out.println(PI);
+        System.out.println(HELLO_MSG);
+    }
+}
