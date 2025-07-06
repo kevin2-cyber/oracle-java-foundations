5298601a33bfeb0421d11aa7890e3ce4a89cc128
Kevin2-cyber <kieduful47@gmail.com>
2025-07-06 14:32:35 +0000

    constants

--- hcm/src/main/java/com/kimikevin/Happiness.java
+++ hcm/src/main/java/com/kimikevin/Happiness.java
@@ -1,5 +1,7 @@
 package com.kimikevin;
 
+import java.util.Scanner;
+
 public class Happiness {
     public static void main(String[] args) {
         double life = 8.6;
@@ -7,5 +9,12 @@ public class Happiness {
         double love = 7.3;
         double hate = 2.3;
         System.out.println("Happiness = " + ((life + laughter) * love - hate / 2.0));
+
+        final Scanner scanner = new Scanner(System.in); // 1
+        final int a;                                    // 2
+        final int b = scanner.nextInt() + a;            // 3
+        final int c = 0;                                // 4
+        c = b;                                          // 5
+        System.out.println(c + 1);                      // 6
     }
 }
