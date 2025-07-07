32a3fcf050bc47188f730fec7e16d60d8d95d6e2
Kevin2-cyber <kieduful47@gmail.com>
2025-07-07 14:33:28 +0000

    removing space

--- hcm/src/main/java/com/kimikevin/Happiness.java
+++ hcm/src/main/java/com/kimikevin/Happiness.java
@@ -10,11 +10,6 @@ public class Happiness {
         double hate = 2.3;
         System.out.println("Happiness = " + ((life + laughter) * love - hate / 2.0));
 
-        final Scanner scanner = new Scanner(System.in); // 1
-        final int a;                                    // 2
-        final int b = scanner.nextInt() + a;            // 3
-        final int c = 0;                                // 4
-        c = b;                                          // 5
-        System.out.println(c + 1);                      // 6
+
     }
 }
