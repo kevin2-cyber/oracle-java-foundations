719264748339eb297454734198e1379aacbbb94a
Kevin2-cyber <kieduful47@gmail.com>
2025-07-09 17:10:28 +0000

    timer

--- hcm/src/main/java/com/kimikevin/Happiness.java
+++ hcm/src/main/java/com/kimikevin/Happiness.java
@@ -1,13 +1,24 @@
 package com.kimikevin;
 
+import java.util.Scanner;
+
 public class Happiness {
     public static void main(String[] args) {
-        double life = 8.6;
-        double laughter = 9.0;
-        double love = 7.3;
-        double hate = 2.3;
-        System.out.println("Happiness = " + ((life + laughter) * love - hate / 2.0));
+//        double life = 8.6;
+//        double laughter = 9.0;
+//        double love = 7.3;
+//        double hate = 2.3;
+//        System.out.println("Happiness = " + ((life + laughter) * love - hate / 2.0));
+//
+//        System.out.println(Math.PI * Math.pow(100,2));\
 
-        System.out.println(Math.PI * Math.pow(100,2));
+        Scanner scanner = new Scanner(System.in);
+        int minutes = scanner.nextInt();
+        int hours = minutes / 60;
+        minutes %= 60;
+        if (hours > 0) {
+            hours++;
+        }
+        System.out.println(minutes + " minutes " + hours + " hours");
     }
 }
