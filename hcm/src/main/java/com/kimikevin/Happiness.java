f06068f0800eb21f12ad3f1442c7bbd9d8d6af8f
Kevin2-cyber <kieduful47@gmail.com>
2025-07-10 00:47:50 +0000

    new

--- hcm/src/main/java/com/kimikevin/Happiness.java
+++ hcm/src/main/java/com/kimikevin/Happiness.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Happiness {
     public static void main(String[] args) {
-//        double life = 8.6;
-//        double laughter = 9.0;
-//        double love = 7.3;
-//        double hate = 2.3;
-//        System.out.println("Happiness = " + ((life + laughter) * love - hate / 2.0));
+        double life = 8.6;
+        double laughter = 9.0;
+        double love = 7.3;
+        double hate = 2.3;
+        System.out.println("Happiness = " + ((life + laughter) * love - hate / 2.0));
 //
 //        System.out.println(Math.PI * Math.pow(100,2));\
 
