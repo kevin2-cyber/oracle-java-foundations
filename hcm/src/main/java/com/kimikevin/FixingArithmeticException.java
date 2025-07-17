ddc7d381d7700e4a6f036477d2184ea90da2af52
Kevin2-cyber <kieduful47@gmail.com>
2025-07-17 16:29:51 +0000

    main

--- hcm/src/main/java/com/kimikevin/FixingArithmeticException.java
+++ hcm/src/main/java/com/kimikevin/FixingArithmeticException.java
@@ -11,35 +11,19 @@ public class FixingArithmeticException {
         int c = scanner.nextInt();
         int d = scanner.nextInt();
 
-//        try {
-//            if(d == 0 && (b + c) == 0) {
-//                System.out.println("Division by zero!");
-//            } else {
-//                int result = a / (b + c) / d;
-//                System.out.println(result);
-//            }
-//        }  catch (ArithmeticException e) {
-//            System.out.println("Division by zero!");
-//        }
 
-        if((b + c) / d == 0) {
+        if (d == 0 || b + c == 0) {
             System.out.println("Division by zero!");
         } else {
-            int result = a / (b + c) / d;
-            System.out.println(result);
+            int denominator = (b + c) / d;
+            if (denominator == 0) {
+                System.out.println("Division by zero!");
+            } else {
+                int result = a / denominator;
+                System.out.println(result);
+            }
         }
 
-        calculator(a, b, c, d);
-
-    }
-
-    public static void calculator(int a, int b, int c, int d) {
-        if((b + c) / d == 0) {
-            System.out.println("Division by zero!");
-        } else {
-            int result = a / (b + c) / d;
-            System.out.println(result);
-        }
     }
 
 }
