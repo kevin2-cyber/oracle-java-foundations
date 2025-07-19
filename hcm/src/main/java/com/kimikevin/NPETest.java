c47a1aefc29d584ea901c2919c9f199953e3c507
Kevin2-cyber <kieduful47@gmail.com>
2025-07-19 00:07:30 +0000

    null pointer exception

--- hcm/src/main/java/com/kimikevin/NPETest.java
+++ hcm/src/main/java/com/kimikevin/NPETest.java
@@ -1,4 +1,11 @@
 package com.kimikevin;
 
 public class NPETest {
+    public static void main(String[] args) {
+        String str = null;
+
+        if (str.equals("abc")) { // it throws an NPE
+            System.out.println("The same");
+        }
+    }
 }
