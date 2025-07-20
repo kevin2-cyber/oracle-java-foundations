cf1fe4a4a4e90edafc7e483450d74c9afbb53470
Kevin2-cyber <kieduful47@gmail.com>
2025-07-20 00:08:33 +0000

    did not print anything

--- hcm/src/main/java/com/kimikevin/NPETest.java
+++ hcm/src/main/java/com/kimikevin/NPETest.java
@@ -4,7 +4,11 @@ public class NPETest {
     public static void main(String[] args) {
         String str = null;
 
-        if (str.equals("abc")) { // it throws an NPE
+//        if (str.equals("abc")) { // it throws an NPE
+//            System.out.println("The same");
+//        }
+
+        if ("abc".equals(str)) { // no NPE here
             System.out.println("The same");
         }
     }
