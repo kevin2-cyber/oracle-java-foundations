f9b47826b7aeed620aad6c5332a13b28fa160e7f
Kevin2-cyber <kieduful47@gmail.com>
2025-07-24 17:04:16 +0000

    Patient Demo

---
+++ hcm/src/main/java/com/kimikevin/PatientDemo.java
@@ -0,0 +1,30 @@
+package com.kimikevin;
+
+public class PatientDemo {
+
+    public static void main(String[] args) {
+
+        Patient john = new Patient();
+
+        john.name = "John";
+        john.age = 30;
+        john.height = 180;
+
+        System.out.println(john.name + " " + john.age + " " + john.height);
+
+        Patient alice = new Patient();
+
+        alice.name = "Alice";
+        alice.age = 22;
+        alice.height = 165;
+
+        System.out.println(alice.name + " " + alice.age + " " + alice.height);
+    }
+}
+
+class Patient {
+
+    String name;
+    int age;
+    float height;
+}
