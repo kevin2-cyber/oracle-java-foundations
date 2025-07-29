a834415c9cfd1e8934f53fe68ec98a8feb3033d1
Kevin2-cyber <kieduful47@gmail.com>
2025-07-29 17:57:05 +0000

    implementing a single instance of Programmer class

--- hcm/src/main/java/com/kimikevin/Telecommunication.java
+++ hcm/src/main/java/com/kimikevin/Telecommunication.java
@@ -1,9 +1,23 @@
 package com.kimikevin;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Telecommunication {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+
+        p.setName("John Elephant");
+        p.setYearOfBirth(1985);
+        p.setAddress("Some street, 15");
+        p.setStartDate(new Date());
+        p.setSalary(500_000L);
+        p.setProgrammingLanguages(new String[] { "Java", "Scala", "Kotlin" });
+
+        System.out.println(p.getName()); // John Elephant
+        System.out.println(p.getSalary()); // 500000
+        System.out.println(Arrays.toString(p.getProgrammingLanguages())); // [Java, Scala, Kotlin]
+    }
 }
 
 class Person {
