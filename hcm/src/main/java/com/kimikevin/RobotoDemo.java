24c1e627395328587af32d4d8698dfa34d782a4f
Kevin2-cyber <kieduful47@gmail.com>
2025-08-05 22:41:29 +0000

    RobotoDemo

---
+++ hcm/src/main/java/com/kimikevin/RobotoDemo.java
@@ -0,0 +1,29 @@
+package com.kimikevin;
+
+public class RobotoDemo {
+    public static void main(String[] args) {
+        RobotoDemo anon = new RobotoDemo();
+        System.out.println(anon);
+    }
+}
+
+class Roboto{
+    String name;
+    String model;
+    int lifetime;
+
+    public Roboto() {
+        this.name = "Anonymous";
+        this.model = "Unknown";
+    }
+
+    public Roboto(String name, String model) {
+        this(name, model, 20);
+    }
+
+    public Roboto(String name, String model, int lifetime) {
+        this.name = name;
+        this.model = model;
+        this.lifetime = lifetime;
+    }
+}
