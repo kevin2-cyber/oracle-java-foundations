4191ee4761019a6262f2f78ec0268c0d61d4e113
Kevin2-cyber <kieduful47@gmail.com>
2025-08-14 12:32:15 +0000

    aug 14

---
+++ hcm/src/main/java/com/kimikevin/Physics.java
@@ -0,0 +1,13 @@
+package com.kimikevin;
+
+public class Physics {
+    /**
+     * The speed of light in a vacuum (m/s)
+     */
+    public static final long SPEED_OF_LIGHT = 299_792_458;
+
+    /**
+     * Electron mass (kg)
+     */
+    public static final double ELECTRON_MASS = 9.1093837e-31;
+}
