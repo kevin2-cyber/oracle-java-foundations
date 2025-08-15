6b10d5f9a814a4b9c4f82148ae1ef0d464c56c34
Kevin2-cyber <kieduful47@gmail.com>
2025-08-15 12:48:30 +0000

    the speed of light

--- hcm/src/main/java/com/kimikevin/Physics.java
+++ hcm/src/main/java/com/kimikevin/Physics.java
@@ -10,4 +10,9 @@ public class Physics {
      * Electron mass (kg)
      */
     public static final double ELECTRON_MASS = 9.1093837e-31;
+
+
+    public static void main(String[] args) {
+        System.out.println(Physics.SPEED_OF_LIGHT);
+    }
 }
