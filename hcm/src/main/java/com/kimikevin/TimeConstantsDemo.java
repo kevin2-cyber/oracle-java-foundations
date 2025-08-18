48090b238092aa5422c7713fb3847b4374681223
Kevin2-cyber <kieduful47@gmail.com>
2025-08-18 13:14:57 +0000

    added 2 static fields

--- hcm/src/main/java/com/kimikevin/TimeConstantsDemo.java
+++ hcm/src/main/java/com/kimikevin/TimeConstantsDemo.java
@@ -6,3 +6,13 @@ public class TimeConstantsDemo {
         // print SECONDS_IN_WEEK
     }
 }
+
+final class TimeConstants {
+    public static final int SECOND = 1;
+    public static final int SECONDS_IN_MINUTE = 60 * SECOND;
+    public static final int SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE;
+    public static final int SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
+    public static final int SECONDS_IN_WEEK = 7 * SECONDS_IN_DAY;
+
+    private TimeConstants() {}
+}
