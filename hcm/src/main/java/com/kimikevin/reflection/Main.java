237adae476da777b9f41c9b9ef0d8707dd6243d2
Kevin2-cyber <kieduful47@gmail.com>
2025-09-09 23:55:45 +0000

    concatenate strings without digits

---
+++ hcm/src/main/java/com/kimikevin/reflection/Main.java
@@ -0,0 +1,21 @@
+package com.kimikevin.reflection;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static String concatenateStringsWithoutDigits(String[] strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : strings) {
+            stringBuilder.append(string.replaceAll("\\d", ""));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = scanner.nextLine().split("\\s+");
+        String result = concatenateStringsWithoutDigits(strings);
+        System.out.println(result);
+    }
+}
