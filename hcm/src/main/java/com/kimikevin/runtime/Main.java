8420ecdfe56380ac7bd7013d8a8d2f44138f2a48
Kevin2-cyber <kieduful47@gmail.com>
2025-09-24 00:55:53 +0000

    adding logical operator

--- hcm/src/main/java/com/kimikevin/runtime/Main.java
+++ hcm/src/main/java/com/kimikevin/runtime/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Object obj = " ";
 
-        if (obj instanceof String) {
-            String str = (String) obj;
-
+        if (obj instanceof String str && str.length() > 0) {
             if (str.isBlank()) {
                 System.out.println("The variable is empty or contains only a whitespace");
             }
