62150f8ac600fde961fc94b2517c1cc2c805aa12
Kevin2-cyber <kieduful47@gmail.com>
2025-10-23 13:56:23 +0000

    change

--- demo/app/src/main/java/com/kimikevin/App.java
+++ demo/app/src/main/java/com/kimikevin/App.java
@@ -74,22 +74,7 @@ public class App {
 
     }
 
-    private static LocalDate getLocalDate() {
-        LocalDate date = LocalDate.of(2017, 11, 25); // 2017-11-25 (25 November 2017)
 
-        int year = date.getYear(); // 2017
-        int month = date.getMonthValue(); // 11
-        int dayOfMonth = date.getDayOfMonth(); // 25
-        int dayOfYear = date.getDayOfYear();  // 329
-
-        int lenOfYear = date.lengthOfYear(); // 365
-        int lenOfMonth = date.lengthOfMonth(); // 30
-
-        LocalDate tomorrow = date.plusDays(1);    // 2017-01-02 (2 January 2017)
-        LocalDate yesterday = date.minusDays(1);  // 2016-12-31 (31 December 2016)
-        LocalDate inTwoYears = date.plusYears(2); // 2019-01-01 (1 January 2019)
-        return date;
-    }
 
     public static String readTextFromFile(String path) throws IOException {
         boolean found = false; // assuming some file finding logic here
