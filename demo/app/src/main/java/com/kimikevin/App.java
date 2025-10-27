c3a176530ad48f0ad1e9b16ea0480d2ce5e7d0d9
Kevin2-cyber <kieduful47@gmail.com>
2025-10-27 21:28:24 +0000

    day

--- demo/app/src/main/java/com/kimikevin/App.java
+++ demo/app/src/main/java/com/kimikevin/App.java
@@ -181,4 +181,21 @@ public class App {
             System.out.println(status);
         }
     }
+
+    public enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+
+        public static void printDay(Day day) {
+            switch(day) {
+                case SUNDAY: System.out.println("It's Sunday!"); break;
+                case MONDAY: System.out.println("It's Monday!"); break;
+                case TUESDAY: System.out.println("It's Tuesday!"); break;
+                case WEDNESDAY: System.out.println("It's Wednesday!"); break;
+                case THURSDAY: System.out.println("It's Thursday!"); break;
+                case FRIDAY: System.out.println("It's Friday!"); break;
+                case SATURDAY: System.out.println("It's Saturday!"); break;
+                // Other cases...
+            }
+        }
+    }
 }
