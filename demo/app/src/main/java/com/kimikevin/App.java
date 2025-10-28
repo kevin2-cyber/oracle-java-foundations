2d177abe5807454f27454d00b928b8f53490689d
Kevin2-cyber <kieduful47@gmail.com>
2025-10-28 22:16:13 +0000

    not exhaustive

--- demo/app/src/main/java/com/kimikevin/App.java
+++ demo/app/src/main/java/com/kimikevin/App.java
@@ -193,6 +193,16 @@ public class App {
         };
 
         System.out.println(taste + ": " + tasteValue);
+
+        int howHungryAmI = 0; // 1 is stuffed, 10 is starving
+
+        int numberOfHamburgersToEat = switch (howHungryAmI) {
+            case 1, 2, 3 -> 0;
+            case 4, 5, 6 -> 1;
+            case 7, 8, 9 -> 2;
+            case 10 -> 3;
+            default -> throw new IllegalStateException("Unexpected value: " + howHungryAmI);
+        };
     }
 
     private enum ThingsToTaste {
