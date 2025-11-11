a6cc2bfd73c108c9f38105c3e80b85e8985137fd
Kevin2-cyber <kieduful47@gmail.com>
2025-11-11 19:08:33 +0000

    comments to modules

--- demo/worldChanger/src/main/java/module-info.java
+++ demo/worldChanger/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module com.kimikevin.worldChanger {
+    // the module's dependencies
     requires com.kimikevin.superUtilities;
 
+    // the packages it explicitly makes available to other modules
     exports com.kimikevin.worldChanger;
 }
