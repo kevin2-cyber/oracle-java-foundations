bfbfdf7b5d87c180a26f8538e745960e14aecb6f
Kevin2-cyber <kieduful47@gmail.com>
2025-11-14 04:31:42 +0000

    hexadecimal form

--- hcm/src/main/java/com/kimikevin/abstractint/Main.java
+++ hcm/src/main/java/com/kimikevin/abstractint/Main.java
@@ -6,5 +6,10 @@ public class Main {
         System.out.println(addition.perform());
         IntBinaryOperation multiplication = new Multiplication(2,4);
         System.out.println(multiplication.perform());
+
+        float hexFloat = 0x5.0p0f;   // 5.0 in hexadecimal form. Here 'f' indicates float
+        double hexDouble = 0xf.8p3; // 15.5 x 2^3 = 124.0 Here 'f' is part of number f.8
+        System.out.println(hexFloat);
+        System.out.println(hexDouble);
     }
 }
