3b8b41d0544a6072d37ef6e9b243ec048e639947
Kevin2-cyber <kieduful47@gmail.com>
2025-11-14 04:18:41 +0000

    main

--- hcm/src/main/java/com/kimikevin/ComplexDemo.java
+++ hcm/src/main/java/com/kimikevin/ComplexDemo.java
@@ -5,13 +5,13 @@ public class ComplexDemo {
         Complex number = new Complex();
         number.real = 10;
         number.image = 4;
-        // math expression of number = 10.0 + 4.0i
 
+        // math expression of number = 10.0 + 4.0i
         Complex anotherNumber = new Complex();
         anotherNumber.real = 6;
         anotherNumber.image = 6;
-        // math expression of anotherNumber = 6.0 + 6.0i
 
+        // math expression of anotherNumber = 6.0 + 6.0i
         number.add(anotherNumber); // number = 16.0 + 10.0i
         number.subtract(anotherNumber);
     }
