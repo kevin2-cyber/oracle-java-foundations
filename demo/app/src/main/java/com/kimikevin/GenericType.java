bab1b48dfc75d9b2ac78b6346de192e2acc8f8b6
Kevin2-cyber <kieduful47@gmail.com>
2025-11-18 12:10:49 +0000

    a sample generic type

---
+++ demo/app/src/main/java/com/kimikevin/GenericType.java
@@ -0,0 +1,30 @@
+package com.kimikevin;
+
+public class GenericType<T> {
+    /**
+     * A field of "some type"
+     */
+    private T value;
+
+    /**
+     * Takes a value of "some type" and assigns it to the field
+     */
+    public GenericType(T value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns a value of "some type"
+     */
+    public T get() {
+        return value;
+    }
+
+    /**
+     * Takes a value of "some type", assigns it to a field and then returns it
+     */
+    public T set(T value){
+        this.value = value;
+        return this.value;
+    }
+}
