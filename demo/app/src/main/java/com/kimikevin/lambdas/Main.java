71e72cda9f6229791caf2e45e065fa3f965ce8aa
Kevin2-cyber <kieduful47@gmail.com>
2025-11-19 11:32:35 +0000

    applying function to each element of the array

--- demo/app/src/main/java/com/kimikevin/lambdas/Main.java
+++ demo/app/src/main/java/com/kimikevin/lambdas/Main.java
@@ -1,5 +1,7 @@
 package com.kimikevin.lambdas;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -8,7 +10,7 @@ public class Main {
         // single line of lambda
         BiFunction<Integer, Integer, Boolean> isDivisible = (x, y) -> x % y == 0;
 
-        // if it has only one argument "()" are optional
+        // if it has only one argument, "()" are optional
         Function<Integer, Integer> adder1 = x -> x + 1;
 
         // with type reference
@@ -27,7 +29,7 @@ public class Main {
         System.out.println(result4Div2);
         System.out.println(result3Div5);
 
-        // returns length of the string
+        // returns the length of the string
         Function<String, Integer> f = s -> s.length();
         printResultOfLambda(f);
 
@@ -59,9 +61,25 @@ public class Main {
         System.out.println(adder100.apply(300));
 
         System.out.println(Operation.binaryOperator.applyAsInt(2,3));
+
+        Scanner scanner = new Scanner(System.in);
+        String method = scanner.nextLine();
+        String[] array = scanner.nextLine().split(" ");
+        applyFunction(array,
+                "lower".equals(method) ? String::toLowerCase :
+                        "upper".equals(method) ? String::toUpperCase :
+                                "new".equals(method) ? String::new :
+                                        "trim".equals(method) ? String::trim : String::intern);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
     }
 
     private static void printResultOfLambda(Function<String, Integer> function) {
         System.out.println(function.apply("HAPPY NEW YEAR 3000!"));
+    }
+
+    public static <T> void applyFunction(T[] array, Function<T, T> func) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = func.apply(array[i]);
+        }
     }
 }
