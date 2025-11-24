7b0ea3de7b236f58b60f9f156253e0912efc7f16
Kevin2-cyber <kieduful47@gmail.com>
2025-11-24 00:18:09 +0000

    splitting the string

--- hcm/src/main/java/com/kimikevin/strings/ProcessingStrings.java
+++ hcm/src/main/java/com/kimikevin/strings/ProcessingStrings.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class ProcessingStrings {
     public static void main(String[] args) {
+        // strings and arrays
         char[] chars = {'A', 'B', 'C', 'D', 'E','F'};
         String stringFromChars = String.valueOf(chars);
 
@@ -19,5 +20,23 @@ public class ProcessingStrings {
         String text = "Hello";
         String[] parts = text.split(""); // {"H", "e", "l", "l", "o"}
         System.out.println(Arrays.toString(parts));
+
+        // splitting the string
+        String sentence = "a long text";
+        String[] words = sentence.split(" "); // {"a", "long", "text"}
+        System.out.println(Arrays.toString(words));
+
+        String number = "+1-213-345-6789";
+        String[] nParts = number.split("-"); // {"+1", "213", "345", "6789"}
+        System.out.println(Arrays.toString(nParts));
+
+        String text2 = "That's one small step for a man, one giant leap for mankind.";
+        String[] parts2 = text2.split(",");
+        System.out.println(Arrays.toString(parts2));
+        // {"That's one small step for a man", " one giant leap for mankind."}
+
+        String text3 = "I'm gonna be a programmer";
+        String[] parts3 = text3.split(" gonna be "); // {"I'm", "a programmer"}
+        System.out.println(Arrays.toString(parts3));
     }
 }
