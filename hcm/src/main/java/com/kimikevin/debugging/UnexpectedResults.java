c1f0c1845d249ceebaa7f2da3fb86482fe84dd48
Kevin2-cyber <kieduful47@gmail.com>
2025-11-26 17:06:32 +0000

    commit with printed statements

--- hcm/src/main/java/com/kimikevin/debugging/UnexpectedResults.java
+++ hcm/src/main/java/com/kimikevin/debugging/UnexpectedResults.java
@@ -2,13 +2,17 @@ package com.kimikevin.debugging;
 
 public class UnexpectedResults {
     public static void main(String[] args) {
+        System.out.println("main() started");
         count(1,10);
+        System.out.println("main() complete");
     }
 
     public static void count(int start, int to) {
+        System.out.println("count() started");
         while (start < to) {
             System.out.println(start);
             start++;
         }
+        System.out.println("count() complete");
     }
 }
