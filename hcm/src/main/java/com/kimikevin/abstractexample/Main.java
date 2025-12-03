7c30a1332437fde726625fb694bdf1f40db6c9e0
Kevin2-cyber <kieduful47@gmail.com>
2025-12-03 14:38:55 +0000

    string modifier

--- hcm/src/main/java/com/kimikevin/abstractexample/Main.java
+++ hcm/src/main/java/com/kimikevin/abstractexample/Main.java
@@ -29,15 +29,44 @@ public class Main {
 //            fibonacciSum += fibonacciCurrent;
 //        }
 
-        // primes and how to find them
-        int greatestPrimeInRange = 2;
-        for (int number = 2; number <= 1000; number++) {
-            if (isPrime(number) && number > greatestPrimeInRange) {
-                greatestPrimeInRange = number;
-            }
-        }
-        System.out.println(greatestPrimeInRange);
+//        // primes and how to find them
+//        int greatestPrimeInRange = 2;
+//        for (int number = 2; number <= 1000; number++) {
+//            if (isPrime(number) && number > greatestPrimeInRange) {
+//                greatestPrimeInRange = number;
+//            }
+//        }
+//        System.out.println(greatestPrimeInRange);
 
+        System.out.println(modifyString("my password is 12345"));
+    }
+
+    // String modifier
+    public static String modifyString(String initialString) {
+        String modString = initialString;
+
+        modString = modString.substring(0,2) + modString.substring(10); // 1
+        if (modString.contains(" ")) { // 2
+            modString += "ABC";
+        }
+
+        int space = modString.indexOf(" ");
+        modString = modString.substring(space + 1) + modString.substring(0, space); // 3
+        if (modString.contains("a")) { // 4
+            modString += "1248";
+        }
+
+         modString = modString.replaceFirst("\\s", "\\$"); // 5
+        if (modString.length() < 15) { // 6
+            modString = new StringBuilder(modString).reverse().toString();
+        }
+
+        modString += "18B20"; // 7
+        int one = modString.indexOf("1");
+        int five = modString.indexOf("5");
+        modString = modString.substring(one + 1) + modString.substring(0, five); // 8
+        modString.substring(0, modString.length() -  4); // 9
+        return modString;
     }
 
     // for checking if a number is prime
