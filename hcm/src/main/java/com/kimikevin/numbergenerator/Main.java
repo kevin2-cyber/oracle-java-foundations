71872d4c6b546641d8e8e944fb8c73fad1315d8f
Kevin2-cyber <kieduful47@gmail.com>
2025-12-17 02:56:13 +0000

    BaseGenerator

--- hcm/src/main/java/com/kimikevin/numbergenerator/Main.java
+++ hcm/src/main/java/com/kimikevin/numbergenerator/Main.java
@@ -2,10 +2,7 @@ package com.kimikevin.numbergenerator;
 
 public class Main {
     public static void main(String[] args) {
-//        BaseNumberGenerator generator = new MagicNumberGenerator(10);
-//        System.out.println(generator.generate());
-        double num1 = 35;
-        double num2 = 23.34;
-        System.out.printf("%f %f", num1, num2);
+        BaseNumberGenerator generator = new MagicNumberGenerator(10);
+        System.out.println(generator.generate());
     }
 }
