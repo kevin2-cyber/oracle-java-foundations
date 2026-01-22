7e3e8dc396c533c8a658ac012db1187a019937b6
Kevin2-cyber <kieduful47@gmail.com>
2026-01-22 17:22:55 +0000

    demonstrates race condition with concurrent deposits

---
+++ demo/app/src/main/java/com/kimikevin/RaceConditionDemo.java
@@ -0,0 +1,32 @@
+package com.kimikevin;
+
+public class RaceConditionDemo {
+    public static int balance = 0;
+
+    public static void main(String[] args) throws InterruptedException{
+        Thread threadA = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                deposit();
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                deposit();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+
+        threadA.join();
+        threadB.join();
+
+        System.out.println("Expected Balance: 20000");
+        System.out.println("Actual Balance: " + balance);
+    }
+
+    public static synchronized void deposit() {
+        balance++;
+    }
+}
