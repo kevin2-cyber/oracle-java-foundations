d4606622c4cf62a2152f18b0b40cc82d0f5160ef
Kevin2-cyber <kieduful47@gmail.com>
2026-02-01 15:13:32 +0000

    performs arithmethic based on an operator argument

--- demo/app/src/main/java/com/kimikevin/ArgumentMain.java
+++ demo/app/src/main/java/com/kimikevin/ArgumentMain.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 public class ArgumentMain {
     public static void main(String[] args) {
-        System.out.println(Arrays.asList(args).indexOf("test"));
-        formatArgs(args);
+//        // checking the index of an argument
+//        System.out.println(Arrays.asList(args).indexOf("test"));
+//        // formatting arguments
+//        formatArgs(args);
+        calculate(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
     }
 
     private static void formatArgs(String[] args) {
@@ -13,4 +16,27 @@ public class ArgumentMain {
             System.out.println(args[i] + "=" + args[i + 1]);
         }
     }
+
+    // Performs arithmetic based on an operator argument
+    private static void calculate(String operator, int a, int b) {
+        switch (operator) {
+            case "+" -> {
+                int sum;
+                sum = a + b;
+                System.out.println(sum);
+            }
+            case "-" -> {
+                int diff;
+                diff = a - b;
+                System.out.println(diff);
+            }
+            case "*" -> {
+                int product;
+                product = a * b;
+                System.out.println(product);
+            }
+            default -> System.out.println("Unknown operator");
+        }
+    }
+
 }
