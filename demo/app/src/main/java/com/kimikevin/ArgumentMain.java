d2cd793eed38a724bfbd5d9d47b985af652ad526
Kevin2-cyber <kieduful47@gmail.com>
2026-02-01 13:40:25 +0000

    main methods

---
+++ demo/app/src/main/java/com/kimikevin/ArgumentMain.java
@@ -0,0 +1,12 @@
+package com.kimikevin;
+
+public class ArgumentMain {
+    public static void main(String[] args) {
+        System.out.println("Method with arguments called!");
+        System.out.println(args[0]);
+    }
+
+    public static void main() {
+        System.out.println("No arguments method called!");
+    }
+}
