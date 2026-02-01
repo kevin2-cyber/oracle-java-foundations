8cdc889e2d445afb9c4ef69cd891d8b40e2738c0
Kevin2-cyber <kieduful47@gmail.com>
2026-02-01 14:58:13 +0000

    format arguments

--- demo/app/src/main/java/com/kimikevin/ArgumentMain.java
+++ demo/app/src/main/java/com/kimikevin/ArgumentMain.java
@@ -5,5 +5,12 @@ import java.util.Arrays;
 public class ArgumentMain {
     public static void main(String[] args) {
         System.out.println(Arrays.asList(args).indexOf("test"));
+        formatArgs(args);
+    }
+
+    private static void formatArgs(String[] args) {
+        for (int i = 0; i < args.length; i +=2) {
+            System.out.println(args[i] + "=" + args[i + 1]);
+        }
     }
 }
