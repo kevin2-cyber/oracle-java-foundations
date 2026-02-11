eb3e4799ba43fc2c01c49b41f901b893c0224d9a
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-02-11 14:50:28 +0000

    get bytes from string

--- demo/app/src/main/java/com/kimikevin/streams/OutputStreamMain.java
+++ demo/app/src/main/java/com/kimikevin/streams/OutputStreamMain.java
@@ -3,14 +3,20 @@ package com.kimikevin.streams;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class OutputStreamMain {
     public static void main(String[] args) throws IOException {
-        String sampleFile = "/Users/kimi-kevin/Desktop/github/oracle-java-foundations/demo/app/src/main/resources/sample.txt";
+        String sampleFile = "/Users/thomas/Desktop/github/oracle-java-foundations/demo/app/src/main/resources/sample.txt";
 
         byte[] data = new byte[]{'s', 't', 'r', 'e', 'a', 'm',};
         OutputStream outputStream = new FileOutputStream(sampleFile, false);
         outputStream.write(data);
         outputStream.close();
+
+        String str = "stream";
+        byte[] strAsBytes = str.getBytes(StandardCharsets.UTF_8);
+        System.out.println(Arrays.toString(strAsBytes));
     }
 }
