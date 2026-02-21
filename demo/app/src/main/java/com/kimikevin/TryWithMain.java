0b0852e3a616d05e779b2effe424e9c8a05e40ba
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-02-21 18:24:26 +0000

    use .env files

--- demo/app/src/main/java/com/kimikevin/TryWithMain.java
+++ demo/app/src/main/java/com/kimikevin/TryWithMain.java
@@ -1,26 +1,39 @@
 package com.kimikevin;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class TryWithMain {
     public static void main(String[] args) {
-//        try {
-//            readFile();
-//        } catch (IOException e) {
-//            System.err.println(e.getMessage());
-//        }
 
-        String language;
-        try (BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in))) {
-            language = buffReader.readLine();
-            System.out.println("I'm learning " + language);
+        Properties properties = new Properties();
+        try(InputStream input = Files.newInputStream(Paths.get(".env"))) {
+            properties.load(input);
+        } catch (Exception e) {
+            System.err.println("Error loading .env file: " + e.getMessage());
+        }
+
+        String filePath = properties.getProperty("READ_FILE_PATH");
+
+        try {
+            readFile(filePath);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+
+//        String language;
+//        try (BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in))) {
+//            language = buffReader.readLine();
+//            System.out.println("I'm learning " + language);
+//        } catch (IOException e) {
+//            System.err.println(e.getMessage());
+//        }
     }
 
-    static void readFile() throws IOException {
-        try(Reader reader = new FileReader("/Users/thomas/Desktop/github/oracle-java-foundations/demo/app/src/main/resources/file.txt")) {
+    static void readFile(String filePath) throws IOException {
+        try(Reader reader = new FileReader(filePath)) {
            int data = reader.read();
               while(data != -1) {
                 System.out.print((char) data);
--- demo/app/src/main/java/com/kimikevin/streams/ReadData.java
+++ demo/app/src/main/java/com/kimikevin/streams/ReadData.java
@@ -3,14 +3,27 @@ package com.kimikevin.streams;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class ReadData {
     /**
      * Reads file into byte array then prints it
      */
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/kimi-kevin/Desktop/github/oracle-java-foundations/demo/app/src/main/resources/file.txt");
+        Properties properties = new Properties();
+        try(InputStream input = Files.newInputStream(Paths.get(".env"))) {
+            properties.load(input);
+        } catch (Exception e) {
+            System.err.println("Error loading .env file: " + e.getMessage());
+        }
+
+        String filePath = properties.getProperty("READ_FILE_PATH");
+
+    File file = new File(filePath);
         byte[] byteArray = new byte[(int) file.length()];
         FileInputStream fis = new FileInputStream(file);
         fis.read(byteArray);
