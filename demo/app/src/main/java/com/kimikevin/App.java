631b196e4ed22cab8208bf135fe8011ef05dd079
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-04-02 23:08:52 +0000

    change

--- demo/app/src/main/java/com/kimikevin/App.java
+++ demo/app/src/main/java/com/kimikevin/App.java
@@ -792,7 +792,7 @@ public class App {
         double average = average(new int[]{1,2,3,4,5});
         System.out.println(average);
 
-        int check = check(new int[]{1, -2, 3, -4, 5});
+        int check = checkPositiveNumbers(new int[]{1, -2, 3, -4, 5});
         System.out.println(check);
 
 
@@ -826,7 +826,7 @@ public class App {
         return (double) result / arr.length;
     }
 
-    public static int check(int[] array) {
+    public static int checkPositiveNumbers(int[] array) {
         if (array.length == 0) {
             return -1;
         }
