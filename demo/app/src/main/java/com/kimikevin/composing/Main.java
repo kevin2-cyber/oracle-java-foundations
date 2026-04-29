ac3fa7dd80d297d87db6661069ff3d6b7d3a1e0c
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-04-29 11:27:57 +0000

    composing predicates

--- demo/app/src/main/java/com/kimikevin/composing/Main.java
+++ demo/app/src/main/java/com/kimikevin/composing/Main.java
@@ -1,6 +1,7 @@
 package com.kimikevin.composing;
 
 import java.util.function.Function;
+import java.util.function.IntPredicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,37 @@ public class Main {
 
         System.out.println("result: " + adder.compose(multiplier).apply(5));
         System.out.println("result: " + adder.andThen(multiplier).apply(5));
+        System.out.println("----------------");
+
+        // composing predicates
+        IntPredicate isOdd = n -> n % 2 != 0;
+
+        System.out.println(isOdd.test(10));
+        System.out.println(isOdd.test(11));
+        System.out.println("----------------");
+
+        IntPredicate lessThan11 = n -> n < 11;
+
+        System.out.println(lessThan11.test(10));
+        System.out.println(lessThan11.test(11));
+        System.out.println("----------------");
+
+        IntPredicate isEven = isOdd.negate();
+        System.out.println(isEven.test(10));
+        System.out.println(isEven.test(11));
+        System.out.println("----------------");
+
+        IntPredicate isOddOrLessThan11 = isOdd.or(lessThan11);
+        System.out.println(isOddOrLessThan11.test(10));
+        System.out.println(isOddOrLessThan11.test(11));
+        System.out.println(isOddOrLessThan11.test(12));
+        System.out.println(isOddOrLessThan11.test(13));
+        System.out.println("----------------");
+
+        IntPredicate isOddAndLessThan11 = isOdd.and(lessThan11);
+        System.out.println(isOddAndLessThan11.test(8));
+        System.out.println(isOddAndLessThan11.test(9));
+        System.out.println(isOddAndLessThan11.test(10));
+        System.out.println(isOddAndLessThan11.test(11));
     }
 }
