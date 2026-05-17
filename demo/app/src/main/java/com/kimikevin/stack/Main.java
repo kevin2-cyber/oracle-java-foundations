64cdc96b1dc0f4cc8e84000a5876305eeacda912
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-05-17 22:25:10 +0000

    deque

---
+++ demo/app/src/main/java/com/kimikevin/stack/Main.java
@@ -0,0 +1,19 @@
+package com.kimikevin.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<String> stack = new ArrayDeque<>();
+
+        stack.offerLast("first");
+        stack.offerLast("second");
+        stack.offerLast("third");
+
+        System.out.println(stack); // [first, second, third]
+
+        System.out.println(stack.pollLast()); // third
+        System.out.println(stack.pollLast()); // second
+        System.out.println(stack.pollLast()); // first
+
+        System.out.println(stack.pollLast()); // null
+    }
+}
