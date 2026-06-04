1c2d84fc16e91e79ee36bbda3d4e76b669640146
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-04 16:14:26 +0000

    More ArrayList methods

--- demo/app/src/main/java/com/kimikevin/arraylist/Main.java
+++ demo/app/src/main/java/com/kimikevin/arraylist/Main.java
@@ -25,5 +25,21 @@ public class Main {
         names.remove("Justin"); // [Laura, Joshua, Marie]
         names.remove(1);        // [Laura, Marie]
         names.clear();          // []
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(1);
+
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+        numbers2.add(100);
+        numbers2.addAll(numbers);
+
+        System.out.println(numbers2.contains(2));    // true
+        System.out.println(numbers2.contains(4));    // false
+        System.out.println(numbers2.indexOf(1));     // 1
+        System.out.println(numbers2.lastIndexOf(1)); // 4
+        System.out.println(numbers2.lastIndexOf(4)); // -1
     }
 }
