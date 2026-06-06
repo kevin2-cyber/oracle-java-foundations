6378657bf83079bc3509a5926469303484187c58
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-06 18:18:47 +0000

    Backward Indexes

---
+++ demo/app/src/main/java/com/kimikevin/list/BackwardIndex.java
@@ -0,0 +1,29 @@
+package com.kimikevin.list;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class BackwardIndex {
+    public static <T> T getElementByIndex(List<T> lst, int index) {
+        return lst.get(index < 0 ? lst.size() + index : index);
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        final List<String> values = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        final int index = Integer.parseInt(scanner.nextLine());
+
+        try {
+            String element = getElementByIndex(values, index);
+            System.out.println(element);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Exception");
+        }
+    }
+}
