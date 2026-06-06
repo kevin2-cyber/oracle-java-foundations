bfdddef75f29c231ca6ecb1e75f208926a700427
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-06 18:24:34 +0000

    Greek Letters

---
+++ demo/app/src/main/java/com/kimikevin/list/App.java
@@ -0,0 +1,35 @@
+package com.kimikevin.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<GreekLetter> letterList = new ArrayList<>();
+
+        letterList.add(new GreekLetter("Gamma",  3));
+        letterList.add(new GreekLetter("Omega", 24));
+        letterList.add(new GreekLetter("Alpha",  1));
+
+        letterList.forEach(System.out::println);
+    }
+
+    static class GreekLetter {
+
+        private String letter;
+        private Integer position;
+
+        public GreekLetter(String letter, Integer position) {
+            this.letter = letter;
+            this.position = position;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "letter='" + letter + '\'' +
+                    ", position=" + position +
+                    '}';
+        }
+    }
+}
