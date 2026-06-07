1912c59ae578fa6d6377d70555f2d56cac03bd68
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-07 18:49:14 +0000

    Immutable maps

---
+++ demo/app/src/main/java/com/kimikevin/map/Main.java
@@ -0,0 +1,14 @@
+package com.kimikevin.map;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> emptyMap = Map.of();
+        Map<String, String> friendPhones = Map.of(
+                "Bob", "+1-202-555-0118",
+                "James", "+1-202-555-0220",
+                "Katy", "+1-202-555-0175"
+        );
+    }
+}
