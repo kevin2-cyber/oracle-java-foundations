8f45d2f3e8f220606a7c711046fc0b10c2c6a99d
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-07 19:09:12 +0000

    HashMap with null value

--- demo/app/src/main/java/com/kimikevin/map/Main.java
+++ demo/app/src/main/java/com/kimikevin/map/Main.java
@@ -1,5 +1,6 @@
 package com.kimikevin.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
@@ -24,5 +25,20 @@ public class Main {
 
         System.out.println(friendPhones.keySet()); // [James, Bob, Katy]
         System.out.println(friendPhones.values()); // [+1-202-555-0220, +1-202-555-0118, +1-202-555-0175]
+
+        Map<Integer, String> products = new HashMap<>();
+
+        products.put(1000, "Notebook");
+        products.put(2000, "Phone");
+        products.put(3000, "Keyboard");
+
+        System.out.println(products); // {2000=Phone, 1000=Notebook, 3000=Keyboard}
+        System.out.println(products.get(1000)); // Notebook
+
+        products.remove(1000);
+        System.out.println(products.get(1000)); // null
+
+        products.putIfAbsent(3000, "Mouse"); // it does not change the current element
+        System.out.println(products.get(3000)); // Keyboard
     }
 }
