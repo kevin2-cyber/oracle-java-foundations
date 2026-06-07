0d10fc84a37965ef6ff06b51659122a8885f8c02
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-07 10:41:53 +0000

    printing the immutable sets

--- demo/app/src/main/java/com/kimikevin/set/Main.java
+++ demo/app/src/main/java/com/kimikevin/set/Main.java
@@ -7,5 +7,9 @@ public class Main {
         Set<String> emptySet = Set.of();
         Set<String> people = Set.of("Larry", "Kenny", "Sabrina");
         Set<Integer> numbers = Set.of(100, 200, 300, 400);
+
+        System.out.println(emptySet); // []
+        System.out.println(people);  // [Kenny, Larry, Sabrina] or another order
+        System.out.println(numbers);  // [400, 200, 300, 100] or another order
     }
 }
