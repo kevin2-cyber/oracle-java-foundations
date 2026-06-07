3f23c2d661af2426bad6d70f32e90a9b36b62d36
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-07 19:10:03 +0000

    HashMap with Default value

--- demo/app/src/main/java/com/kimikevin/map/Main.java
+++ demo/app/src/main/java/com/kimikevin/map/Main.java
@@ -36,7 +36,7 @@ public class Main {
         System.out.println(products.get(1000)); // Notebook
 
         products.remove(1000);
-        System.out.println(products.get(1000)); // null
+        System.out.println(products.getOrDefault(1000, "Unknown product")); // null
 
         products.putIfAbsent(3000, "Mouse"); // it does not change the current element
         System.out.println(products.get(3000)); // Keyboard
