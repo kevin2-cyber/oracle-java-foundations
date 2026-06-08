a473a6e4f23219fe8f1256008a8b571e4e21fffd
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-08 19:04:00 +0000

    custom object to compare

---
+++ demo/app/src/main/java/com/kimikevin/comparable/Car.java
@@ -0,0 +1,47 @@
+package com.kimikevin.comparable;
+
+public class Car {
+    private int number;
+    private String model;
+    private String color;
+    private int weight;
+
+    public Car(int number, String model, String color, int weight) {
+        this.number = number;
+        this.model = model;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
