34a9cd762f1c7f06a01776f31d950928fa30f5a2
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-09 22:00:31 +0000

    putting messages in a collection

--- demo/app/src/main/java/com/kimikevin/comparator/Main.java
+++ demo/app/src/main/java/com/kimikevin/comparator/Main.java
@@ -1,5 +1,19 @@
 package com.kimikevin.comparator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        List<Message> messages = new ArrayList<>();
+
+        messages.add(new Message("Hello"));
+        messages.add(new Message("humans!"));
+        messages.add(new Message("We"));
+        messages.add(new Message("came"));
+        messages.add(new Message("in"));
+        messages.add(new Message("peace!"));
+
+        System.out.println(messages);
+    }
 }
