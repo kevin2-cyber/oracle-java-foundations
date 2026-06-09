2684fcc2c408aa0d717250edbbaf7f5a19dbdee0
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-09 22:38:03 +0000

    Message Content Comparator

---
+++ demo/app/src/main/java/com/kimikevin/comparator/MessageContentComparator.java
@@ -0,0 +1,10 @@
+package com.kimikevin.comparator;
+
+import java.util.Comparator;
+
+public class MessageContentComparator implements Comparator<Message> {
+    @Override
+    public int compare(Message o1, Message o2) {
+        return 0;
+    }
+}
