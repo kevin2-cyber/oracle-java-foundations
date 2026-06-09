47e90fe53909919e51e9b7a9e6189037faba1902
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-09 23:10:36 +0000

    create a new list of messages to try different sorting criteria

--- demo/app/src/main/java/com/kimikevin/comparator/Main.java
+++ demo/app/src/main/java/com/kimikevin/comparator/Main.java
@@ -7,12 +7,14 @@ public class Main {
     public static void main(String[] args) {
         List<Message> messages = new ArrayList<>();
 
-        messages.add(new Message("Hello"));
-        messages.add(new Message("humans!"));
-        messages.add(new Message("We"));
-        messages.add(new Message("came"));
-        messages.add(new Message("in"));
-        messages.add(new Message("peace!"));
+        messages.add(new Message("Alien", "Hello humans!",
+                32, "2034-03-25"));
+        messages.add(new Message("Pirate", "All hands on deck!",
+                -2, "2034-01-05"));
+        messages.add(new Message("User765214", "Bump!",
+                1, "2033-02-17"));
+        messages.add(new Message("Unregistered", "This message was marked as spam",
+                -18, "2033-01-14"));
 
         System.out.println(messages);
 
