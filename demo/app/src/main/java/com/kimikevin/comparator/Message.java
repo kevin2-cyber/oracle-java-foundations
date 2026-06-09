da2b74c022b516cc8f9462945d23ca5488cf3cf7
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-06-09 23:08:22 +0000

    extend Message class to implement sorting

--- demo/app/src/main/java/com/kimikevin/comparator/Message.java
+++ demo/app/src/main/java/com/kimikevin/comparator/Message.java
@@ -1,18 +1,39 @@
 package com.kimikevin.comparator;
 
-public class Message {
-    private final String content;
+import java.time.LocalDate;
 
-    public Message(String content) {
+public class Message {
+    private final String from;
+    private final String content;
+    private final LocalDate created;
+    private int likes;
+
+    public Message(String from, String content, int likes, String created) {
+        this.from = from;
         this.content = content;
+        this.likes = likes;
+        this.created = LocalDate.parse(created);
+    }
+
+    public String getFrom() {
+        return from;
     }
 
     public String getContent() {
         return content;
     }
 
+    public int getLikes() {
+        return likes;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
     @Override
     public String toString() {
-        return content;
+        return created.toString() + " " + from + " wrote: "
+                + content + " (" + likes + " likes)";
     }
 }
